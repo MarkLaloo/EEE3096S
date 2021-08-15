5ec27dab7f18678f71d7ed4a3d8431edc1848b17
Mark Laloo <llxmar003@myuct.ac.za>
2021-08-15 19:16:37 +0200

    LLXMAR003's main.c commit

---
+++ main.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main(){
+	int a, b, sum;
+	
+	printf("Enter a value for a: ");
+	scanf("%d", &a);
+	
+	printf("Enter a value for b: ");
+	scanf("%d", &b);
+	
+	sum = a + b;
+	
+	printf("The sum of a and b is %d \n.",sum);
+}
