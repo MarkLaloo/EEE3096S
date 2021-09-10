992995036aecfd4827b051c3dc980289432bbf14
Mark Laloo <llxmar003@myuct.ac.za>
2021-09-10 21:05:56 +0200

    cleaned up p3 a little

--- Prac-03/p3.py
+++ Prac-03/p3.py
@@ -123,24 +123,17 @@ def setup():
 # Load high scores
 def fetch_scores():
     # get however many scores there are
-    #sleep(0.01)
-    #eeprom.populate_mock_scores()
     sleep(0.01)
-    #block1 = eeprom.read_block(0,4)
     score_count=eeprom.read_byte(0)
-    #score_count=block1[0]
 
     sleep(0.01)
-    #tempCount = score_count
     # Get the scores
     counter=1
     scores=[]
     
     boolScore=True
     while boolScore==True:
-        #print('test2')
         tempArr=eeprom.read_block(counter,4)
-        
         if tempArr[0]!=0:
             name=''
             blockData=eeprom.read_block(counter,4)
@@ -153,15 +146,12 @@ def fetch_scores():
             output=[name,tempScore]
             scores.append(output)
             counter+=1
-            #score_count+=1
-            #eeprom.write_byte(0,score_count)
-            
+
         else:
             boolScore=False
     # convert the codes back to ascii
     # return back the results
-    #print(score_count)
-    #print(scores)
+
     return score_count, scores
 
 
@@ -172,10 +162,7 @@ def save_scores():
     # include new score
     global playerName
     global playerScore
-    print(playerName)
-    tempName=playerName[:3]
     playerName[:3]
-    #playerScore=3
     nameAndScore=[str(playerName),int(playerScore)]
     scores.append(nameAndScore)
     sleep(0.01)
@@ -184,8 +171,6 @@ def save_scores():
         data_to_write = []
         a=0
         for letter in score[0]:
-            #if letter<3:
-            
             if a<3:
                 data_to_write.append(ord(letter))
             else:
@@ -194,13 +179,7 @@ def save_scores():
         data_to_write.append(score[1])
         eeprom.write_block(i+1, data_to_write)
     score_count=eeprom.read_byte(0)
-    #print(score_count)
     eeprom.write_byte(0,score_count+1)
-    #print(eeprom.read_block(1,4))
-    #print(eeprom.read_block(2,4))
-    #print(eeprom.read_block(3,4))
-    #print(eeprom.read_block(4,4))
-    #print(eeprom.read_block(5,4))
     # sort
     # update total amount of scores
     # write new scores
@@ -217,8 +196,6 @@ def btn_increase_pressed(channel):
     # Increase the value shown on the LEDs
     # You can choose to have a global variable store the user's current guess, 
     # or just pull the value off the LEDs when a user makes a guess
-    #print('test1')
-    #currentGuess=0
     global currentGuess
     currentGuess+=1
     if currentGuess==0:
@@ -287,13 +264,11 @@ def btn_guess_pressed(channel):
         buzzer_pwm.ChangeDutyCycle(0)
         led_pwm.stop()
         buzzer_pwm.stop()
-        #currentGuess=0
         GPIO.cleanup()
         setup()
         menu()
     elif buttonTime>0.1:
         playerScore+=1
-        #print('btn submit works')
         accuracy_leds()
         global playerName
     
@@ -331,7 +306,6 @@ def accuracy_leds():
     # - The % brightness should be directly proportional to the % "closeness"
     # - For example if the answer is 6 and a user guesses 4, the brightness should be at 4/6*100 = 66%
     # - If they guessed 7, the brightness would be at ((8-7)/(8-6)*100 = 50%
-    #print('accuracy_leds')
     global value
     if currentGuess<value:
         led_duty = currentGuess/value*100
@@ -354,7 +328,6 @@ def trigger_buzzer():
     # If the user is off by an absolute value of 3, the buzzer should sound once every second
     # If the user is off by an absolute value of 2, the buzzer should sound twice every second
     # If the user is off by an absolute value of 1, the buzzer should sound 4 times a second
-    #print('trigger_buzzer')
     temp = currentGuess-value
     #buzzer_pwm.start(50)
     if abs(temp)==1:
