b68bcd27d83484b27137bf332c14672144c66b29
Mark <llxmar003@myuct.ac.za>
2021-10-04 01:18:51 +0200

    made some minor comments

--- Prac-04/prac4.py
+++ Prac-04/prac4.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 #import RPi.GPIO as GPIO
+#tried to setup nicely using function blocks
 
 btn = 18
 
@@ -20,15 +21,16 @@ def setup():
     # crate the mcp object
     global mcp
     mcp = MCP.MCP3008(spi, cs)
-
+    
     global sampleTime
     sampleTime = 5
-    print("{:<10} {:<15} {:<15} {:<10} {:<15}".format('Runtime','Temp Reading','Temp','','Light Reading'))
+    print("{:<10} {:<15} {:<15} {:<10} {:<15}".format('Runtime','Temp Reading','Temp','','Light Reading')) #nice format
     
+    #below is button setup, not complete
     btn=digitalio.DigitalInOut(board.D18)
     btn.direction = digitalio.Direction.INPUT
     btn.pull = digitalio.Pull.UP
-
+    #to change time, not yet used, relies on btn 
     if btn.value == False:
         change_sample_time()
 
@@ -39,6 +41,7 @@ def setup():
 
 
 def change_sample_time():
+    #cycles sample time
     global sampleTime
     if sampleTime==1:
         sampleTime=5
@@ -64,6 +67,9 @@ def print_thread():
 
     tempSens = AnalogIn(mcp, MCP.P1)
     LDR = AnalogIn(mcp,MCP.P2)
+
+    #voltage-0.5 bc 0deg Celsius at 500mV according to the datasheet
+    #thereafter is linear where 10mV=1deg celsius so x100 converts to temp
     temp = (tempSens.voltage-0.5)*100    
 
     #print('Raw ADC tempSens Value: ', tempSens.value)
@@ -72,7 +78,7 @@ def print_thread():
     
     #print('Raw ADC LDR Value: ', LDR.value)
     #print('LDR ADC Voltage: '+str(LDR.voltage))
-    print("{:<10} {:<15} {:<15} {:<10} {:<15}".format(str(dur),str(tempSens.value),str(temp)[:10],'C',str(LDR.value)))
+    print("{:<10} {:<15} {:<15} {:<10} {:<15}".format(str(dur),str(tempSens.value),str(temp)[:10],'C',str(LDR.value))) # fancy print
     
 
     
