d89707ee95e484735cb5ca9ee1bed124e48e69d0
Mark <llxmar003@myuct.ac.za>
2021-10-04 15:24:43 +0200

    ímplemented button

--- Prac-04/prac4.py
+++ Prac-04/prac4.py
@@ -5,14 +5,19 @@ import threading
 from datetime import datetime
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
-#import RPi.GPIO as GPIO
+#import gpiozero
+import RPi.GPIO as GPIO
 #tried to setup nicely using function blocks
 
-btn = 18
+btn = 24
+totalDur=0
 
 def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(btn,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(btn,GPIO.RISING,callback=change_sample_time,bouncetime=200)
+
     # create the spi bus
-    #GPIO.setmode(GPIO.BOARD)
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
     # create the cs (chip select)
@@ -23,25 +28,26 @@ def setup():
     mcp = MCP.MCP3008(spi, cs)
     
     global sampleTime
-    sampleTime = 5
+    sampleTime = 1
     print("{:<10} {:<15} {:<15} {:<10} {:<15}".format('Runtime','Temp Reading','Temp','','Light Reading')) #nice format
     
     #below is button setup, not complete
-    btn=digitalio.DigitalInOut(board.D18)
-    btn.direction = digitalio.Direction.INPUT
-    btn.pull = digitalio.Pull.UP
+    #btn=digitalio.DigitalInOut(board.D18)
+    #btn.direction = digitalio.Direction.INPUT
+    #btn.pull = digitalio.Pull.UP
     #to change time, not yet used, relies on btn 
-    if btn.value == False:
-        change_sample_time()
-
+    #btn = gpiozero.Button(24)
+    #btn.when_pressed = change_sample_time
+    
     global start
     start=datetime.now()
     # create an analog input channel on pin 0
 
 
 
-def change_sample_time():
+def change_sample_time(channel):
     #cycles sample time
+    #print('sample time changed')
     global sampleTime
     if sampleTime==1:
         sampleTime=5
@@ -49,19 +55,22 @@ def change_sample_time():
         sampleTime=10
     elif sampleTime==10:
         sampleTime=1
+    #print('sample time = ',sampleTime)
 
 def print_thread():
-
+    #global btn
+    #btn.when_pressed = change_sample_time
 
     thread = threading.Timer(sampleTime,print_thread)
     thread.daemon = True
     
     global start
+    global totalDur
     end=datetime.now()
     dur = end - start
     dur = dur.seconds
     start=end
-
+    totalDur+=dur
     thread.start()
     
 
@@ -78,9 +87,8 @@ def print_thread():
     
     #print('Raw ADC LDR Value: ', LDR.value)
     #print('LDR ADC Voltage: '+str(LDR.voltage))
-    print("{:<10} {:<15} {:<15} {:<10} {:<15}".format(str(dur),str(tempSens.value),str(temp)[:10],'C',str(LDR.value))) # fancy print
+    print("{:<10} {:<15} {:<15} {:<10} {:<15}".format(str(totalDur)+'s',str(tempSens.value),str(temp)[:10],'C',str(LDR.value))) # fancy print
     
-
     
 
 if __name__ == "__main__":
